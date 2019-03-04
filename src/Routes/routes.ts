c2cb32c28b5189c8776c46bc9f3756ddfe6acd11
srijan <srijanreddy98@gmail.com>
2019-03-04 10:29:18 +0530

    Changes in pipe line

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -1,5 +1,6 @@
 import { Influencer } from '../db/models/models';
 // import * as data from '../data.json';
+
 let routes = (app) => {
     app.get('/api', (req, res) => {
         res.send('Server is up and running');
@@ -17,64 +18,40 @@ let routes = (app) => {
             err => res.send(err)
         );
     });
+
+// TODO: Merge /api/noInterests into this route
     app.post('/api/interests', (req, res) => {
         let desc = req.body.desc ? -1 : 1; 
-        let pipeline1 =  [
-            {
-                "$project": {
-                    "stats.engagement.avgCommentsRatio": 1,
-                    "stats.engagement.avgLikesRatio": 1,
-                    "stats.interests": 1,
-                    "username": 1,
-                    "fullName": 1,
-                    "picture": 1,
-                    "biography": 1,
-                    "followerCount": 1,
-                    "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
-                    "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
-                }
-            },
-            {
-                "$sort": {"comments":desc}
-            }            
-        ];
-        let pipeline2 =  [
-            {
-                "$project": {
-                    "stats.engagement.avgCommentsRatio": 1,
-                    "stats.engagement.avgLikesRatio": 1,
-                    "stats.interests": 1,
-                    "username": 1,
-                    "fullName": 1,
-                    "picture": 1,
-                    "biography": 1,
-                    "followerCount": 1,
-                    "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
-                    "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
-                }
-            },
-            {
-                "$sort": {"likes":desc}
-            }            
-        ];
+        let pipeline = {
+            "$project": {
+                "stats.engagement.avgCommentsRatio": 1,
+                "stats.engagement.avgLikesRatio": 1,
+                "stats.interests": 1,
+                "username": 1,
+                "fullName": 1,
+                "picture": 1,
+                "biography": 1,
+                "followerCount": 1,
+                "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
+                "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
+            }
+        };
+        //TODO: Lots of repeated code, make it DRY
         if (req.body.shouldContainOne) {
             if (req.body.sortBase == 0) {
-                // console.log(1);
                 Influencer.find({'stats.interests': {$in: req.body.interests}}).sort([['followerCount', 'descending']]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 1) {
-                // console.log(2);
-                Influencer.aggregate([{$match:{'stats.interests': {$in: req.body.interests}}},...pipeline1]).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([{$match:{'stats.interests': {$in: req.body.interests}}},pipeline,{"$sort": {"comments":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 2) {
-                // console.log(3);
-                Influencer.aggregate([{$match:{'stats.interests': {$in: req.body.interests}}},...pipeline2]).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([{$match:{'stats.interests': {$in: req.body.interests}}},pipeline, {"$sort": {"likes":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
@@ -82,22 +59,19 @@ let routes = (app) => {
             }
         }  else {
             if (req.body.sortBase == 0) {
-                // console.log(4);
                 Influencer.find({'stats.interests': {$all: req.body.interests}}).sort([['followerCount', 'descending']]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 1) {
-                // console.log(5);
-                Influencer.aggregate([{$match:{'stats.interests': {$all: req.body.interests}}},...pipeline1]).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([{$match:{'stats.interests': {$all: req.body.interests}}},pipeline, {"$sort": {"comments":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 2) {
-                // console.log(6);
-                Influencer.aggregate([{$match:{'stats.interests': {$all: req.body.interests}}},...pipeline2]).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([{$match:{'stats.interests': {$all: req.body.interests}}},pipeline, {"$sort": {"likes":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
@@ -106,7 +80,7 @@ let routes = (app) => {
     });
     app.post('/api/noInterests', (req, res) => {
         let desc = req.body.desc ? -1 : 1; 
-        let pipeline1 =  [
+        let pipeline =
             {
                 "$project": {
                     "stats.engagement.avgCommentsRatio": 1,
@@ -120,48 +94,22 @@ let routes = (app) => {
                     "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
                     "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
                 }
-            },
-            {
-                "$sort": {"comments":desc}
-            }            
-        ];
-        let pipeline2 =  [
-            {
-                "$project": {
-                    "stats.engagement.avgCommentsRatio": 1,
-                    "stats.engagement.avgLikesRatio": 1,
-                    "stats.interests": 1,
-                    "username": 1,
-                    "fullName": 1,
-                    "picture": 1,
-                    "biography": 1,
-                    "followerCount": 1,
-                    "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
-                    "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
-                }
-            },
-            {
-                "$sort": {"likes":desc}
-            }            
-        ];
+            };
         if (req.body.shouldContainOne) {
             if (req.body.sortBase == 0) {
-                // console.log(1);
                 Influencer.find().sort([['followerCount', 'descending']]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 1) {
-                // console.log(2);
-                Influencer.aggregate(pipeline1).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([pipeline, {"$sort": {"comments":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 2) {
-                // console.log(3);
-                Influencer.aggregate(pipeline2).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([pipeline,{"$sort": {"likes":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
@@ -169,22 +117,19 @@ let routes = (app) => {
             }
         }  else {
             if (req.body.sortBase == 0) {
-                // console.log(4);
                 Influencer.find().sort([['followerCount', 'descending']]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 1) {
-                // console.log(5);
-                Influencer.aggregate(pipeline1).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([pipeline, {"$sort": {"comments":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
             }
             if (req.body.sortBase == 2) {
-                // console.log(6);
-                Influencer.aggregate(pipeline2).skip(req.query.offset*10).limit(10).then(
+                Influencer.aggregate([pipeline, {"$sort": {"likes":desc}}]).skip(req.query.offset*10).limit(10).then(
                     docs => res.send(docs),
                     err => res.send(err)
                 );
@@ -203,31 +148,6 @@ let routes = (app) => {
     //         err => res.send(err)
     //     );
     // });
-    app.get('/api/check', (req, res) => {
-        let pipeline =  [
-            {
-                "$project": {
-                    "stats.engagement.avgCommentsRatio": 1,
-                    "stats.engagement.avgLikesRatio": 1,
-                    "stats.interest": 1,
-                    "username": 1,
-                    "fullName": 1,
-                    "picture": 1,
-                    "biography": 1,
-                    "followerCount": 1,
-                    "comments": { "$multiply": [ "$stats.engagement.avgCommentsRatio", "$followerCount" ] },
-                    "likes": { "$multiply": [ "$stats.engagement.avgLikesRatio", "$followerCount" ] }
-                }
-            },
-            {
-                "$sort": {"likes":-1}
-            }            
-        ];
-    Influencer.aggregate(pipeline).then(
-        docs => res.send(docs),
-        err => res.send(err)
-    );
-    });
 }
 
 export {routes};
